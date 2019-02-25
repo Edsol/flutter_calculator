5f14415a68759deafaa820cade61bd2636f37802
Edsol <edoardo.soloperto@gmail.com>
2019-02-25 20:26:18 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appTitle,
-      theme: new ThemeData.dark(),
+      // theme: new ThemeData.dark(),
       home: MyHomePage(),
     );
   }
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -9,9 +9,25 @@ class MyHomePage extends StatefulWidget {
 
 class HomePageState extends State<MyHomePage> {
 
+  String numbers_value ="0";
+
+  String first_number = "0";
+
+
+  Column _createColumn(List<String> values) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: 
+      values.map(
+        (value) => _createButton(value,_clear)
+      ).toList()
+    );
+  }
+
   Widget _createButton(String number, Function() f) {
     return MaterialButton(
       height: 100.0,
+      minWidth: 102.5,
       child: Text(number,
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
       textColor: Colors.black,
@@ -20,11 +36,10 @@ class HomePageState extends State<MyHomePage> {
     );
   }
 
-  Column _createColumn(List<String> values){
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: values.map((value) => _createButton(value,_clear)).toList()
-    );
+  _addNumber(String new_char){
+    setState(() {
+      numbers_value += new_char;
+    });
   }
 
   _clear() {}
@@ -44,19 +59,26 @@ class HomePageState extends State<MyHomePage> {
             children: <Widget>[
               new Container(
                 constraints: BoxConstraints.expand(
-                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 +
-                      100.0,
+                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 60.0,
                 ),
                 alignment: Alignment.bottomRight,
                 color: Colors.white,
                 child: Text(
-                  "1234",
+                  numbers_value,
                   style: TextStyle(fontSize: 50, color: Colors.black),
                   textAlign: TextAlign.right,
                 ),
               ),
-              _createColumn(["/","X","-","+","="]),
-              // _createColumn(["/","X","-","+","="])
+              new Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  _createColumn(["AC", "7", "4", "1", " "]),
+                  _createColumn(['C', "8", "5", "2", "0"]),
+                  _createColumn(["%", "9", "6", "3", "."]),
+                  _createColumn(["/", "X", "-", "+", "="]),
+                ],
+              )
             ],
           ),
         ));
